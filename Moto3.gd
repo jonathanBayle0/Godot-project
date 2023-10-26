c21eee92057c5a822195db72276f31cc57d5c81a
jonathanBayle <jonathan.bayle002@gmail.com>
2023-10-26 11:42:44 +0200

    Première version du jeu
    - Scène principale : préparation pour le terrain du jeu mais non
      fonctionnel
    - Scène test : test des fonctionnalité sans dégradé la scène principale
    - Dans la scène de test, la moto se déplace et réussi a franchir des
      obstacle

---
+++ Moto3.gd
@@ -0,0 +1,15 @@
+extends VehicleBody3D
+
+const MAX_STEER = 0.8
+const ENGINE_POWER = 300
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	steering = move_toward(steering, Input.get_axis("ui_right", "ui_left") * MAX_STEER, delta * 2.5)
+	engine_force = Input.get_axis("ui_up", "ui_down") * ENGINE_POWER
+	
