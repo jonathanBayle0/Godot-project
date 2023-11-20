96948a050ac57eaef8c2094b64b5b959d40fb6a2
jonathanBayle <jonathan.bayle002@gmail.com>
2023-11-20 11:01:10 +0100

    Test sur l'avancement de la moto

--- car.gd
+++ car.gd
@@ -3,6 +3,8 @@ extends VehicleBody3D
 const MAX_STEER = 0.8
 const ENGINE_POWER = 3000
 
+var start_pos = position
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -10,6 +12,10 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	steering = move_toward(steering, Input.get_axis("ui_right", "ui_left") * MAX_STEER, delta * 2.5)
+	# steering = move_toward(steering, Input.get_axis("ui_right", "ui_left") * MAX_STEER, delta * 2.5)
 	engine_force = Input.get_axis("ui_up", "ui_down") * ENGINE_POWER
+	
+	if Input.is_action_pressed("reset"):
+		position = start_pos
+	
 	
